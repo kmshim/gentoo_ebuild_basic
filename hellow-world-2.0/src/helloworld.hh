5335788ce7b2ce772f06a1605fdc30363df6b00d
kmshim <mwtkmshim@gmail.com>
2019-04-20 21:11:41 +0900

    Add hello world example written by c++

---
+++ hellow-world-2.0/src/helloworld.hh
@@ -0,0 +1,17 @@
+#ifndef EXAMPLE_HELLOWORD_H
+#define EXAMPLE_HELLOWORD_H
+
+#include <iostream>
+
+class HelloWorld {
+   	public:
+   		HelloWorld();
+		virtual ~HelloWorld();
+        void run();
+								
+	protected:
+		virtual void my_world();	
+											
+};
+
+#endif
---
+++ hellow-world-2.0/src/main.cpp
@@ -0,0 +1,10 @@
+#include "helloworld.hh"
+
+int main(int argc, char *argv[]){
+
+   	HelloWorld helloworld;
+	helloworld.run();
+
+	return 0;
+}
+
